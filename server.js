dcd7deeb9a3ef9a22f55edecb19ca89f7bace94b
James Gooding <jamescrgooding@gmail.com>
2026-05-03 13:43:02 +0100

    Add local mockup preview

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3456;
+const DIR = __dirname;
+
+const mime = { '.html': 'text/html', '.css': 'text/css', '.js': 'application/javascript', '.png': 'image/png', '.jpg': 'image/jpeg', '.svg': 'image/svg+xml' };
+
+http.createServer((req, res) => {
+  let filePath = path.join(DIR, req.url === '/' ? 'mockups.html' : req.url);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[path.extname(filePath)] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log('Server running on port ' + PORT));
